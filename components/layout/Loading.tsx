86f6a7f73c97112d4eeb74343dfb1ac66f98db14
Manuel Lamon <leo0lamon@gmail.com>
2024-05-23 11:24:56 -0400

    bugfix

--- components/layout/Loading.tsx
+++ components/layout/Loading.tsx
@@ -1,8 +1,6 @@
 'use client'
 import React, { useContext,useEffect } from "react";
 import { useRender } from "@/context/render/renderProvider";
-import Lottie from "lottie-react";
-import lotti from "@/images/vO7RppFOYL.json";
 
 function Loading() {
 
@@ -17,18 +15,18 @@ function Loading() {
     <>
       <input type="checkbox" checked={loader} disabled id="my-modal-3" className="modal-toggle" />
       <div className="modal">
-      {/* <div className="sk-chase">
+      <div className="sk-chase">
             <div className="sk-chase-dot"></div>
             <div className="sk-chase-dot"></div>
             <div className="sk-chase-dot"></div>
             <div className="sk-chase-dot"></div>
             <div className="sk-chase-dot"></div>
             <div className="sk-chase-dot"></div>
-          </div> */}
-      <div className=" w-[20vw] bg-white rounded-2xl p-10 border-primary border-2 ">
+          </div>
+     {/*  <div className=" w-[20vw] bg-white rounded-2xl p-10 border-primary border-2 ">
         <Lottie animationData={lotti} loop={true} width={300} height={300}/>
         <p className=" text-center text-xl font-bold animate-pulse w-full "> Tu cerebro se esta entrenando.</p>
-      </div>
+      </div> */}
       </div>
     </>
   );
